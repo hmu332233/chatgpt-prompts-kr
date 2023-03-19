a280ad2724db00f112e27d1e85bd843816b17782
Minung Han <hmu332233@gmail.com>
2023-03-20 03:11:06 +0900

    추후 데이터 형식에 맞춰 변경

--- packages/site/src/components/Card.tsx
+++ packages/site/src/components/Card.tsx
@@ -5,14 +5,15 @@ type Props = {
   title: string;
   contents: string;
   category: string;
+  upvotes: number;
 };
 
-function Card({ title, contents, category }: Props) {
+function Card({ title, contents, category, upvotes }: Props) {
   return (
     <Collapse
       title={<h4>{title}</h4>}
       subtitle={category}
-      contentLeft={<Upvote count={5} onClick={console.log} />}
+      contentLeft={<Upvote count={upvotes} onClick={console.log} />}
       bordered
     >
       <Text>{contents}</Text>
--- packages/site/src/components/CardList.tsx
+++ packages/site/src/components/CardList.tsx
@@ -8,6 +8,7 @@ type Props = {
     title: string;
     contents: string;
     category: string;
+    upvotes: number;
   }>;
 };
 
@@ -15,12 +16,13 @@ function CardList({ items }: Props) {
   const { categoryId } = useParams();
   return (
     <div className="flex flex-col gap-8">
-      {items.slice(0, parseInt(categoryId!)).map((item) => (
+      {items.map((item) => (
         <Card
           key={item.id}
           title={item.title}
           contents={item.contents}
           category={item.category}
+          upvotes={item.upvotes}
         />
       ))}
     </div>
